67b372f66d771d81f538a14858e4ded89ee9010c
nnaakkaaii <nakai-yu623@g.ecc.u-tokyo.ac.jp>
2023-09-17 14:53:10 +0900

    feat: add command-line arguments for multi-resolution settings

--- dvf-calculator/script.py
+++ dvf-calculator/script.py
@@ -5,52 +5,59 @@ import argparse
 from tqdm import tqdm
 
 
-def compute_dvf(fixed_image, moving_image):
-    """
-    Compute the 3D DVF between two images.
-    
-    Args:
-    - fixed_image: The image that will be used as the reference.
-    - moving_image: The image that will be aligned to the fixed_image.
-    
-    Returns:
-    - dvf: The displacement vector field as a numpy array.
-    """
-    # Define the registration method
-    registration_method = sitk.ImageRegistrationMethod()
+class DVFCalculator:
+    def __init__(self, shrink_factors, smoothing_sigmas):
+        self.__shrink_factors = shrink_factors
+        self.__smoothing_sigmas = smoothing_sigmas
 
-    # Similarity metric settings
-    registration_method.SetMetricAsMeanSquares()
-    registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
-    registration_method.SetMetricSamplingPercentage(0.01)
+    def compute(self, fixed_image, moving_image):
+        """
+        Compute the 3D DVF between two images.
+        
+        Args:
+        - fixed_image: The image that will be used as the reference.
+        - moving_image: The image that will be aligned to the fixed_image.
+        
+        Returns:
+        - dvf: The displacement vector field as a numpy array.
+        """
+        # Define the registration method
+        registration_method = sitk.ImageRegistrationMethod()
 
-    # Multi-resolution settings
-    registration_method.SetShrinkFactorsPerLevel(shrinkFactors=[4, 2, 1])
-    registration_method.SetSmoothingSigmasPerLevel(smoothingSigmas=[2, 1, 0])
-    registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
+        # Similarity metric settings
+        registration_method.SetMetricAsMeanSquares()
+        registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
+        registration_method.SetMetricSamplingPercentage(0.01)
 
-    # Optimizer settings
-    registration_method.SetOptimizerAsGradientDescent(learningRate=1.0, numberOfIterations=100)
-    registration_method.SetOptimizerScalesFromPhysicalShift()
+        # Multi-resolution settings
+        registration_method.SetShrinkFactorsPerLevel(shrinkFactors=self.__shrink_factors)
+        registration_method.SetSmoothingSigmasPerLevel(smoothingSigmas=self.__smoothing_sigmas)
+        registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
 
-    # Set initial transform (identity)
-    initial_transform = sitk.TranslationTransform(fixed_image.GetDimension())
-    registration_method.SetInitialTransform(initial_transform, inPlace=True)
+        # Optimizer settings
+        registration_method.SetOptimizerAsGradientDescent(learningRate=1.0, numberOfIterations=100)
+        registration_method.SetOptimizerScalesFromPhysicalShift()
 
-    # Register the images
-    final_transform = registration_method.Execute(fixed_image, moving_image)
+        # Set initial transform (identity)
+        initial_transform = sitk.TranslationTransform(fixed_image.GetDimension())
+        registration_method.SetInitialTransform(initial_transform, inPlace=True)
 
-    # Compute the DVF
-    dvf = sitk.TransformToDisplacementField(final_transform, 
-                                            outputPixelType=sitk.sitkVectorFloat64,
-                                            size=fixed_image.GetSize(),
-                                            outputOrigin=fixed_image.GetOrigin(),
-                                            outputSpacing=fixed_image.GetSpacing(),
-                                            outputDirection=fixed_image.GetDirection())
-    return sitk.GetArrayFromImage(dvf)
+        # Register the images
+        final_transform = registration_method.Execute(fixed_image, moving_image)
+
+        # Compute the DVF
+        dvf = sitk.TransformToDisplacementField(final_transform, 
+                                                outputPixelType=sitk.sitkVectorFloat64,
+                                                size=fixed_image.GetSize(),
+                                                outputOrigin=fixed_image.GetOrigin(),
+                                                outputSpacing=fixed_image.GetSpacing(),
+                                                outputDirection=fixed_image.GetDirection())
+        return sitk.GetArrayFromImage(dvf)
 
 
-def main(directory):
+def main(directory, shrink_factors, smoothing_sigmas):
+    dvf_calculator = DVFCalculator(shrink_factors, smoothing_sigmas)
+
     # Recursively search for npz files
     npz_files = [os.path.join(root, file) 
                  for root, dirs, files in os.walk(directory)
@@ -68,7 +75,7 @@ def main(directory):
             fixed_img = sitk.GetImageFromArray(fixed_img_array)
             moving_img = sitk.GetImageFromArray(moving_img_array)
 
-            dvf = compute_dvf(fixed_img, moving_img)
+            dvf = dvf_calculator.compute(fixed_img, moving_img)
             dvfs.append(dvf)
 
         dvfs = np.stack(dvfs, axis=0)
@@ -81,5 +88,10 @@ def main(directory):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Compute DVFs for 4D-CT data in a directory.")
     parser.add_argument("directory", type=str, help="Directory containing the 4D-CT npz files.")
+    parser.add_argument("--shrink-factors", type=int, nargs='+', default=[8, 4, 2, 1],
+                        help="Shrink factors for multi-resolution approach.")
+    parser.add_argument("--smoothing-sigmas", type=int, nargs='+', default=[4, 2, 1, 0],
+                        help="Smoothing sigmas for multi-resolution approach.")
+    
     args = parser.parse_args()
     main(args.directory)
