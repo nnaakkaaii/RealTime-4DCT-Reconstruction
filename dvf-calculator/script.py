730eebc23137ac774865ed339d3e557583343625
nnaakkaaii <nakai-yu623@g.ecc.u-tokyo.ac.jp>
2023-09-17 14:41:23 +0900

    fix: error around VectorFloat64

--- dvf-calculator/script.py
+++ dvf-calculator/script.py
@@ -2,6 +2,7 @@ import SimpleITK as sitk
 import numpy as np
 import os
 import argparse
+from tqdm import tqdm
 
 
 def compute_dvf(fixed_image, moving_image):
@@ -13,7 +14,7 @@ def compute_dvf(fixed_image, moving_image):
     - moving_image: The image that will be aligned to the fixed_image.
     
     Returns:
-    - dvf: The displacement vector field.
+    - dvf: The displacement vector field as a numpy array.
     """
     # Define the registration method
     registration_method = sitk.ImageRegistrationMethod()
@@ -41,38 +42,39 @@ def compute_dvf(fixed_image, moving_image):
 
     # Compute the DVF
     dvf = sitk.TransformToDisplacementField(final_transform, 
-                                            sitk.VectorFloat64, 
-                                            fixed_image.GetSize(),
-                                            fixed_image.GetOrigin(),
-                                            fixed_image.GetSpacing(),
-                                            fixed_image.GetDirection())
-    return dvf
+                                            outputPixelType=sitk.sitkVectorFloat64,
+                                            size=fixed_image.GetSize(),
+                                            outputOrigin=fixed_image.GetOrigin(),
+                                            outputSpacing=fixed_image.GetSpacing(),
+                                            outputDirection=fixed_image.GetDirection())
+    return sitk.GetArrayFromImage(dvf)
 
 def main(directory):
     # Recursively search for npz files
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith('.npz'):
-                filepath = os.path.join(root, file)
-                # Load npz file
-                data = np.load(filepath)['arr_0']
-                
-                dvfs = []
-                for t in range(9):
-                    fixed_img_array = data[t]
-                    moving_img_array = data[t + 1]
-                    
-                    fixed_img = sitk.GetImageFromArray(fixed_img_array)
-                    moving_img = sitk.GetImageFromArray(moving_img_array)
-                    
-                    dvf = compute_dvf(fixed_img, moving_img)
-                    dvfs.append(sitk.GetArrayFromImage(dvf))
-                
-                dvfs = np.stack(dvfs, axis=0)
-                
-                # Save the DVF as a npz file
-                save_path = os.path.join(root, file.replace('.npz', '_dvf.npz'))
-                np.savez(save_path, arr_0=dvfs)
+    npz_files = [os.path.join(root, file) 
+                 for root, dirs, files in os.walk(directory)
+                 for file in files if file.endswith(".npz")]
+
+    for filepath in tqdm(npz_files, desc="Processing files"):
+        # Load npz file
+        data = np.load(filepath)['arr_0']
+
+        dvfs = []
+        for t in range(9):
+            fixed_img_array = data[t]
+            moving_img_array = data[t + 1]
+
+            fixed_img = sitk.GetImageFromArray(fixed_img_array)
+            moving_img = sitk.GetImageFromArray(moving_img_array)
+
+            dvf = compute_dvf(fixed_img, moving_img)
+            dvfs.append(dvf)
+
+        dvfs = np.stack(dvfs, axis=0)
+
+        # Save the DVF as a npz file
+        save_path = os.path.join(os.path.dirname(filepath), os.path.basename(filepath).replace('.npz', '_dvf.npz'))
+        np.savez(save_path, arr_0=dvfs)
 
 
 if __name__ == "__main__":
